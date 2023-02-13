53ade610445af61f2e1d28927e8b974c99842f0c
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-13 12:16:50 +0700

    Initial commit

---
+++ src/main/java/com/bayu/springboot2jwt/SpringBoot2JwtApplication.java
@@ -0,0 +1,13 @@
+package com.bayu.springboot2jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2JwtApplication.class, args);
+	}
+
+}
